997bb1d431a76e7d1783c9a42410c140fdbdffab
tidusete <jordicidsierra@gmail.com>
2025-04-12 15:03:55 +0200

    FEATURE: Enabling CICD + ruff

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
 import psycopg2
-from psycopg2 import sql, OperationalError
+from psycopg2 import OperationalError
 from datetime import date
 from typing import Optional
 from app.config import DATABASE_URL
