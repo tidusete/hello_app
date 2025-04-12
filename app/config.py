6078cd4c44160516de809df82dbb37fd659ee9c2
tidusete <jordicidsierra@gmail.com>
2025-04-12 17:11:32 +0200

    FEATURE: Added dotenv library for local development

--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
 import os
 
+# For local development
+if os.getenv("ENV", "development") == "development":
+    from dotenv import load_dotenv
+    load_dotenv()
+
 # Access values safely
 DATABASE_URL: str = os.getenv("DATABASE_URL")
